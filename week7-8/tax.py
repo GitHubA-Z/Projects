026e26fb3d660cd6fb382b21a0e5187ad3010b68
GitHubA-Z <59344360+GitHubA-Z@users.noreply.github.com>
2020-03-07 00:51:14 +0400

    Update tax.py

--- week7-8/tax.py
+++ week7-8/tax.py
@@ -29,12 +29,34 @@
 
 #------------------ @start:zaid------------------------------
 
-
-
-
-
-
-
+class TaxCalc:
+  def __init__(self,name,salary):
+    self.name=name
+    self.salary=salary
+    
+  def tax(self):
+    if self.salary <10000:
+      return 0
+    elif self.salary<20000:
+      return self.salary*0.10
+    elif self.salary<40000:
+      return self.salary*0.20
+    else:
+      return self.salary*0.30
+    
+  def details(self):
+    return f"{self.name} is getting ${self.salary} as his salary, so he has to pay ${self.tax()} as a salary tax.\n\nThank you for using the Tax Calculator🌹"
+  
+eName=str(input("please insert the employee name here:\n"))
+print("\n")
+eSalary=int(input(f"please insert {eName}'s salary here:\n"))
+  
+  
+emp=TaxCalc(eName,eSalary)
+print("\n")
+print("Tax=$",emp.tax())
+print("\n")
+print(emp.details())
 
 #-------------------- @end:zaid------------------------------
 
